be70dbe7122d950f31561c244167cceaa48666b2
Karam Beloneh <karamp238@gmail.com>
2023-04-01 14:54:39 +0300

    Continue with ui, building some logic

--- lib/services/get_ayat.dart
+++ lib/services/get_ayat.dart
@@ -3,14 +3,10 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class GetAyat {
-  Future<List<String>> getAyatF() async {
+  Future<Map<String, dynamic>> getAyatF() async {
     http.Response response = await http
         .get(Uri.parse('http://api.alquran.cloud/v1/quran/ar.alafasy'));
     Map<String, dynamic> data = jsonDecode(response.body);
-    List<String> ayat = [];
-    for (var i = 0; i < 114; i++) {
-      ayat.add(data['data']['surahs'][i]['name']);
-    }
-    return ayat;
+    return data;
   }
 }
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:quran/services/get_ayat.dart';
 
 class Home extends StatelessWidget {
@@ -8,27 +6,54 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Future<List<String>> ayat = GetAyat().getAyatF();
-    print(ayat);
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'القرآن الكريم',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
-        ),
-      ),
-      body: ListView.builder(
-        itemCount: 20,
-        itemBuilder: (context, index) {
-          Container(
-            color: Colors.amber,
-            height: 100,
-            width: MediaQuery.of(context).size.width,
-            child: Text('Test'),
-          );
-        },
-      ),
-    );
+    Future<Map<String, dynamic>> data = GetAyat().getAyatF();
+    return FutureBuilder(
+        future: data,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<String> ayat = [];
+            for (var i = 0; i < 114; i++) {
+              ayat.add('Test');
+            }
+
+            return Scaffold(
+              appBar: AppBar(
+                centerTitle: true,
+                title: Text(
+                  'القرآن الكريم',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
+                ),
+              ),
+              body: ListView.builder(
+                itemCount: ayat.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Container(
+                        color: Colors.amber,
+                        height: 100,
+                        width: MediaQuery.of(context).size.width,
+                        child: Center(
+                            child: Text(
+                          ayat[index],
+                          style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        )),
+                      ),
+                      Divider(
+                        color: Colors.brown,
+                        thickness: 17,
+                      )
+                    ],
+                  );
+                },
+              ),
+            );
+          } else {
+            return CircularProgressIndicator();
+          }
+        });
   }
 }
